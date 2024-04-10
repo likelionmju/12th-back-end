82f78d6dd6b5d65880a8a92ce925219d4549a262
SeongWon1178 <major0025@naver.com>
2024-04-10 17:54:24 +0900

    week03_김성원

---
+++ week03/김성원/bj5.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class bj5{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        int search=0;
+
+        int N= sc.nextInt();
+
+        int[] num=new int [N];
+        for(int i=0; i<N;i++){
+            num[i]=sc.nextInt();
+        }
+
+        int v=sc.nextInt();
+
+        for(int i=0;i<N;i++){
+            if(num[i]==v){
+                search=search+1;
+            }
+        }
+        System.out.print(search);
+    }
+}

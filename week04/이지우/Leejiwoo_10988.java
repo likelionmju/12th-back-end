da1adf0bf6d69f7eb15928a1b370ba5702a85391
Jiwoo-Leee <dlwldn0126@gmail.com>
2024-05-01 09:15:52 +0900

    week04_이지우

--- week04/이지우/Leejiwoo_10988.java
+++ week04/이지우/Leejiwoo_10988.java
@@ -1,11 +1,24 @@
-
+import java.util.Scanner;
 public class Leejiwoo_10988 {
-	public static boolean isPal(String str) {
-		
-	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        boolean isPalindrome = true;
 
-	}
+        int length = word.length();
+        for (int i=0;i<length/2;i++) { // 일단 반만 비교
+            if (word.charAt(i) != word.charAt(length-1-i)) { //문자열의 처음과 끝부터 비교
+                isPalindrome = false;
+                break;
+            }
+        }
 
+        if (isPalindrome) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+        scanner.close();
+	}//pass
 }
--- week04/이지우/Leejiwoo_1152.java
+++ week04/이지우/Leejiwoo_1152.java
@@ -3,27 +3,13 @@ public class Leejiwoo_1152 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String sentence = scanner.nextLine();
-		int cnt=1;
-		for(int i=0;i<sentence.length();i++) {
-			char ch = sentence.charAt(i);
-			int lastIndex = sentence.length() - 1;
-			
-			if(sentence.charAt(0)==' ' || sentence.charAt(lastIndex)==' ') {
-				cnt--;
-				if(ch == ' ') {
-					cnt++; 
-				}
-				
-			}
-			if(ch == ' ') {
-				
-				cnt++;
-			}
-			
+		String sentence = scanner.nextLine().trim();
+		if(sentence.isEmpty()) {
+			System.out.println('0');
 		}
-		System.out.println(cnt);
+		else
+			System.out.println(sentence.split(" ").length);
 		scanner.close();
 	}
-
+//pass
 }

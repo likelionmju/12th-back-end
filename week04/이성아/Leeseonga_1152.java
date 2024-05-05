ada255975b8229985bacef748a775bb9f1fc437b
이성아 <2saritaum@gmail.com>
2024-05-05 18:27:39 +0900

    week04 이성아

--- week04/이성아/Leeseonga_1152.java
+++ week04/이성아/Leeseonga_1152.java
@@ -5,6 +5,7 @@ public class Leeseonga_1152 {
         Scanner scn = new Scanner(System.in);
         String sentence = scn.nextLine();
         scn.close();
+        sentence = sentence.trim();
 
         System.out.println(sentence.split(" ").length);
     }

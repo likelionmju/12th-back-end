9729496d2c6da1cbcb1067d4083095c8cb208e8b
이성아 <2saritaum@gmail.com>
2024-05-05 18:20:52 +0900

    week04 이성아

--- week04/이성아/Leeseonga_2738.java
+++ week04/이성아/Leeseonga_2738.java
@@ -1,18 +1,42 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Leeseonga_10809 {
+public class Leeseonga_2738 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("영어 단어를 입력하시오.");
-        String word = scn.next();
+        int row = scn.nextInt();
+        int column = scn.nextInt();
 
-        List<String> alpabet = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
+        int[][] arrayA = new int[row][column];
+        int[][] arrayB = new int[row][column];
+        int[][] arraySum = new int[row][column];
+        int sum = 0;
 
-        System.out.println("가장 앞에 있는 알파벳 위치 출력 (미포함시 -1) :");
-        System.out.println("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z");
-        for (int i = 0; i < alpabet.size(); i++) {
-            String compare = alpabet.get(i);
-            System.out.print(word.indexOf(compare) + " ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                arrayA[i][j] = scn.nextInt();
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                arrayB[i][j] = scn.nextInt();
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                int a = arrayA[i][j];
+                int b = arrayB[i][j];
+                sum = a + b;
+                arraySum[i][j] = sum;
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print(arraySum[i][j] + " ");
+            }
+        System.out.println();
         }
     }
 }

705a0bc2419ef2aa358e7c6e83e77694efc7b12f
wooseob <dksdntjq3285@naver.com>
2024-07-01 17:08:24 +0900

    week_09 7월 1일

---
+++ week09/안우섭/LogAspect.java
@@ -0,0 +1,24 @@
+package aspect;
+
+import java.util.logging.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Component
+@Aspect
+public class LogAspect {
+  Logger logger = LoggerFactory.getLogger(LogAspect.class);
+  @Around("@annotation(LogExecutionTime)")
+  public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+    StopWatch stopWatch = new StopWatch();
+    stopWatch.start();
+    Object proceed = joinPoint.proceed();
+    stopWatch.stop();
+    logger.info(stopWatch.prettyPrint());
+    return proceed;
+  }
+}

7e5e3c7680b28467a2b77f02a38765197de3c09f
LeeJaeHyeok97 <hazardous10@naver.com>
2024-07-02 16:24:04 +0900

    week_08

---
+++ week08/이재혁/SpringConfig.java
@@ -0,0 +1,11 @@
+@Configuration
+public class SpringConfig {
+  private final MemberRepository memberRepository;
+  public SpringConfig(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
+  @Bean
+  public MemberService memberService() {
+    return new MemberService(memberRepository);
+  }
+}

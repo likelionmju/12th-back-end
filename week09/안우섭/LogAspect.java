5a9513e3240cd92b5e8a35feb0e3a512c2f797e4
wooseob <dksdntjq3285@naver.com>
2024-07-10 13:34:15 +0900

    week09_안우섭

--- week09/안우섭/LogAspect.java
+++ week09/안우섭/LogAspect.java
@@ -1,24 +1,25 @@
-package aspect;
+package com.spring.proxy;
 
-import java.util.logging.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
+import org.aspectj.lang.annotation.Aspect;
+
 
 @Component
 @Aspect
 public class LogAspect {
-  Logger logger = LoggerFactory.getLogger(LogAspect.class);
-  @Around("@annotation(LogExecutionTime)")
-  public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-    StopWatch stopWatch = new StopWatch();
-    stopWatch.start();
-    Object proceed = joinPoint.proceed();
-    stopWatch.stop();
-    logger.info(stopWatch.prettyPrint());
-    return proceed;
-  }
+    Logger logger = LoggerFactory.getLogger(LogAspect.class);
+    @Around("@annotation(LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Object proceed = joinPoint.proceed();
+        stopWatch.stop();
+        logger.info(stopWatch.prettyPrint());
+        return proceed;
+    }
 }
